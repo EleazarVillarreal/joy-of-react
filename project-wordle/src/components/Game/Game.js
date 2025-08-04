b23f0d293e8ea62a8917ce2b3ea17ce36d87eb30
Eleazar Villarreal <EleazarVillarreal13@gmail.com>
2025-08-04 08:14:07 -0500

    initial commit for project wordle

--- project-wordle/src/components/Game/Game.js
+++ project-wordle/src/components/Game/Game.js
@@ -1,56 +1,15 @@
-import React, { useState } from 'react'
+import React from 'react';
 
-import { WORDS } from '../../data'
-import { sample } from '../../utils'
-import { NUM_OF_GUESSES_ALLOWED } from '../../constants'
-import Guesses from '../Guesses/Guesses'
-import GuessInput from '../GuessInput/GuessInput'
-import GameOverBanner from '../GameOverBanner/GameOverBanner'
+import { sample } from '../../utils';
+import { WORDS } from '../../data';
+
+// Pick a random word on every pageload.
+const answer = sample(WORDS);
+// To make debugging easier, we'll log the solution in the console.
+console.info({ answer });
 
 function Game() {
-  const [guesses, setGuesses] = useState([])
-  const [gameWon, setGameWon] = useState(false)
-  const [gameOver, setGameOver] = useState(false)
-  const [answer, setAnswer] = useState(() => sample(WORDS))
-
-  console.log({ answer })
-
-  const handleGuess = (guess) => {
-    const allGuesses = [...guesses, guess]
-
-    if (guess.toUpperCase() === answer.toUpperCase()) {
-      setGameWon(true)
-      setGameOver(true)
-    }
-
-    if (allGuesses.length === NUM_OF_GUESSES_ALLOWED) {
-      setGameOver(true)
-    }
-
-    setGuesses([...allGuesses])
-  }
-
-  const restartGame = () => {
-    setGuesses([])
-    setGameWon(false)
-    setGameOver(false)
-    setAnswer(sample(WORDS))
-  }
-
-  return (
-    <>
-      <Guesses answer={answer} guesses={guesses} />
-      <GuessInput gameOver={gameOver} handleGuess={handleGuess} />
-      {gameOver && (
-        <GameOverBanner
-          answer={answer}
-          gameWon={gameWon}
-          numOfGuesses={guesses.length}
-          restartGame={restartGame}
-        />
-      )}
-    </>
-  )
+  return <>Put a game here!</>;
 }
 
-export default Game
+export default Game;
