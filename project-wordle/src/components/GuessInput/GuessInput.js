4c680fa71d85123776d9cb52edd06bb5e5dbc4fa
EleazarVillarreal <EleazarVillarreal13@gmail.com>
2026-05-23 15:53:34 -0500

    exercise 1 - wordle

---
+++ project-wordle/src/components/GuessInput/GuessInput.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+
+function GuessInput() {
+  const [guess, setGuess] = useState('');
+
+  function handleChange(event) {
+    setGuess(event.target.value.toUpperCase());
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(guess);
+    setGuess('');
+  }
+
+  return (
+    <form className="guess-input-wrapper" onSubmit={handleSubmit}>
+      <label htmlFor="guess-input">Enter Guess:</label>
+      <input
+        required
+        minLength={5}
+        maxLength={5}
+        pattern="[a-zA-Z]{5}"
+        title="Enter a 5 letter word."
+        id="guess-input"
+        type="text"
+        value={guess}
+        onChange={handleChange}
+      />
+    </form>
+  );
+}
+
+export default GuessInput;
