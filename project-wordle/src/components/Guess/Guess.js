3758e2f87098c365413bef024f2393686e1727b9
EleazarVillarreal <EleazarVillarreal13@gmail.com>
2026-05-24 11:13:43 -0500

    exercise 3 - wordle

---
+++ project-wordle/src/components/Guess/Guess.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { range } from '../../utils';
+
+function Guess({ value }) {
+  return (
+    <p className="guess">
+      {range(5).map((index) => (
+        <span className="cell" key={index}>
+          {value ? value[index] : ''}
+        </span>
+      ))}
+    </p>
+  );
+}
+
+export default Guess;
