d5353e728f239af3f4229702e9a987bac53f60bb
EleazarVillarreal <EleazarVillarreal13@gmail.com>
2026-05-25 09:45:26 -0500

    exercise 4 - wordle

--- project-wordle/src/components/Guess/Guess.js
+++ project-wordle/src/components/Guess/Guess.js
@@ -1,15 +1,26 @@
 import React from 'react';
 
+import { checkGuess } from '../../game-helpers';
 import { range } from '../../utils';
 
-function Guess({ value }) {
+function Guess({ value, answer }) {
+  const result = checkGuess(value, answer);
+
   return (
     <p className="guess">
-      {range(5).map((index) => (
-        <span className="cell" key={index}>
-          {value ? value[index] : ''}
-        </span>
-      ))}
+      {range(5).map((index) => {
+        if (!result) {
+          return <span className="cell" key={index} />;
+        }
+
+        const { letter, status } = result[index];
+
+        return (
+          <span className={`cell ${status}`} key={index}>
+            {letter}
+          </span>
+        );
+      })}
     </p>
   );
 }
